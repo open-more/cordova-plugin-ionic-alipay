8ba559e62d1b1073a046b2acb0633621be79e26f
gerrit <michaeltang@openmore.org>
2017-11-28 12:55:15 +0800

    fix import error

--- www/alipay.js
+++ www/alipay.js
@@ -1,9 +1,6 @@
-cordova.define("ionic-cordova-plugin-alipay.Alipay", function(require, exports, module) {
-    var exec = require('cordova/exec');
-    
-    module.exports.payment = function(payInfo, success, error) {
+var exec = require('cordova/exec');
+module.exports = {
+    payment: function(payInfo, success, error) {
         exec(success, error, "Alipay", "payment", [payInfo]);
-    };
-    
-});
-    
+    },
+}; 
